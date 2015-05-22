1410a9a3f5ef4bb1af3db860b24c23b528feef38
Sara OC <saramoo@hotmail.com>
2015-05-22 16:06:36 +0100

    tiny refactor

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -12,18 +12,18 @@ feature 'users' do
     assert last_response.ok?
   end
 
+  scenario 'are created when route receives a name' do
+    post '/users', name: 'JoeBob'
+    expect(JSON.parse(last_response.body)['name']).to eq 'JoeBob'
+    # curl -X POST -d "name=Steve" http://localhost:3000/users
+    # curl -X POST -d "name=Sara" https://makersvisitorapi.herokuapp.com/users
+  end
+
   scenario 'can be accessed via get request' do
     new_user = User.create(name: 'Bob')
     get "/users/#{new_user.id}"
     expect(JSON.parse(last_response.body)['name']).to eq 'Bob'
     # curl -X GET http://localhost:3000/users/2
     # curl -X GET https://makersvisitorapi.herokuapp.com/users/1
-  end
-
-  scenario 'are created when route receives a name' do
-    post '/users', name: 'JoeBob'
-    expect(JSON.parse(last_response.body)['name']).to eq 'JoeBob'
-    # curl -X POST -d "name=Steve" http://localhost:3000/users
-    # curl -X POST -d "name=Sara" https://makersvisitorapi.herokuapp.com/users
   end
 end
