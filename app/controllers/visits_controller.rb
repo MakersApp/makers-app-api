fc4cf458caeb2855a1243b648bc7a5ac2cc9d404
Sara OC <saramoo@hotmail.com>
2015-05-22 11:12:20 +0100

    slack integration when visit id is hit

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -11,21 +11,21 @@ class VisitsController < ApplicationController
     @visit = Visit.new(visit_params)
     @visit.save
     render json: @visit
-    # notify_slack
   end
 
   def update
     visit = Visit.find(params[:id])
     visit.update(visit_params)
+    notify_slack
   end
 
   def visit_params
     params.permit(:user_id, :checkedin)
   end
 
-  # def notify_slack
-  #   notifier = Slack::Notifier.new "https://hooks.slack.com/services/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", channel: '#private_soc_channel', username: "webhookbot", icon_emoji: ":sanjsanj:", link_names: 1
-  #   notifier.ping "Hello @soc, Dan has arrived"
-  #   render json: notifier
-  # end
+  def notify_slack
+    notifier = Slack::Notifier.new "https://hooks.slack.com/services/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", channel: '#private_soc_channel', username: "webhookbot", icon_emoji: ":sanjsanj:", link_names: 1
+    notifier.ping "Hello @soc, Dan has arrived"
+    render json: notifier
+  end
 end
--- spec/features/visits_feature_spec.rb
+++ spec/features/visits_feature_spec.rb
@@ -28,8 +28,10 @@ feature 'visits' do
     # curl -X PATCH -d "checkedin=true" http://localhost:3000/users/2/visits/1
   end
 
-  xscenario 'sends a message to Slack' do
-    post 'users/1/visits/', user_id: 1
+  scenario 'sends a message to Slack' do
+    user = User.create(name: 'James')
+    visit = Visit.create(user_id: user.id)
+    patch "users/#{user.id}/visits/#{visit.id}", checkedin: true
     expect(JSON.parse(last_response.body).to_s).to include ":sanjsanj:"
     # curl -X POST --data-urlencode 'payload={"channel": "#visitors", "username": "webhookbot", "text": "Hey @james, you sure are looking great today.", "icon_emoji": ":sanjsanj:",  "link_names": 1}' https://hooks.slack.com/services/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
   end
