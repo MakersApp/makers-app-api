adb64073754fcdbe2652091a29a05daea62652b2
Richard Lynch <rjlynch86@googlemail.com>
2015-05-29 13:44:57 +0100

    updates travis file

--- spec/features/feedback_feature_spec.rb
+++ spec/features/feedback_feature_spec.rb
@@ -1,13 +1,11 @@
 require 'rails_helper'
 
 feature 'feedback' do
-    User.all.each do |user|
-      puts user.phone_id
-    end
+
   scenario 'is sent via API call' do
     stub_request(:any, Rails.application.secrets.google_form_url)
 
-    post '/feedback', phone_id: 'asdf', enjoyed: true, questions: 'No thanks', email: 'test@t.com'
+    post '/feedback', phoneID: 'asdf', enjoyed: true, questions: 'No thanks', email: 'test@t.com'
     expect(WebMock).to have_requested(:post, Rails.application.secrets.google_form_url)
   end
 
